b8f15a1b96e546a1113a15914baf74970a5e662d
Harry Morgan <psyhm6@nottingham.ac.uk>
2024-03-01 19:56:46 +0000

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Harry Morgan on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
